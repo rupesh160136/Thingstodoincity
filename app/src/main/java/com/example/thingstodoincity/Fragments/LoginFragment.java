e21dc4b5780e5b6045d987b45ed3db8b5cd86c24
rupesh160136 <rupeshrohit1956@gmail.com>
2019-05-29 22:13:27 +0545

    add login and signup fragment

--- app/src/main/java/com/example/thingstodoincity/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/thingstodoincity/Fragments/LoginFragment.java
@@ -1,18 +1,29 @@
 package com.example.thingstodoincity.Fragments;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.thingstodoincity.Dashboard;
 import com.example.thingstodoincity.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener  {
+    private EditText txtusername,txtpassword;
+    private Button btnlogin;
 
 
     public LoginFragment() {
@@ -23,8 +34,52 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view= inflater.inflate(R.layout.fragment_login, container, false);
+        txtusername=view.findViewById(R.id.txtusername);
+        txtpassword=view.findViewById(R.id.txtpassword);
+        btnlogin=view.findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(this);
+        return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        if (validate()) {
+            checkUser();
+
+        }
+    }
+    private boolean validate(){
+        boolean checkvalidate=true;
+        if(TextUtils.isEmpty(txtusername.getText().toString())){
+            txtusername.setError("Username is required");
+            txtusername.requestFocus();
+            checkvalidate=false;
+        }
+        if(TextUtils.isEmpty(txtpassword.getText().toString())){
+            txtpassword.setError("Password is required");
+            txtpassword.requestFocus();
+            checkvalidate=false;
+        }
+        return  checkvalidate;
+
+    }
+    private void checkUser(){
+        SharedPreferences sharedPreferences=getActivity().getSharedPreferences("User",MODE_PRIVATE);
+        String Username=sharedPreferences.getString("username","");
+        String Password=sharedPreferences.getString("password","");
+        if(Username.equals(txtusername.getText().toString()) && Password.equals(txtpassword.getText().toString())){
+
+            Toast.makeText(getActivity(),"Successfull ",Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(getActivity(), Dashboard.class);
+            startActivity(intent);
+
+        }
+        else{
+            Toast.makeText(getActivity(),"Either Username or Password is Incorrect ",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
 }
---
+++ app/src/main/java/com/example/thingstodoincity/ViewPagerActivity.java
@@ -0,0 +1,29 @@
+package com.example.thingstodoincity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.thingstodoincity.Adapter.ViewPagerAdapter;
+import com.example.thingstodoincity.Fragments.LoginFragment;
+import com.example.thingstodoincity.Fragments.SignupFragment;
+
+public class ViewPagerActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+        tabLayout=findViewById(R.id.tabId);
+        viewPager=findViewById(R.id.viewpager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LoginFragment(),"Login");
+        adapter.addFragment(new SignupFragment(),"Register");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
